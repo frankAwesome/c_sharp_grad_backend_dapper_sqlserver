968af27281a7a05c8cd43f5542704ca2b7c29af6
Eddie Maune <eddie.maune@vcontractor.co.za>
2020-05-28 18:20:41 +0200

    SMS feature

--- c_sharp_grad_backend/Controllers/DonationsController.cs
+++ c_sharp_grad_backend/Controllers/DonationsController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using c_sharp_grad_backend.Data;
@@ -39,6 +41,7 @@ namespace c_sharp_grad_backend.Controllers
         public async Task<IActionResult> AddDonation(Donations donation)
         {
             var cell = "";
+            var firstname = "";
 
             using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
             {
@@ -50,7 +53,28 @@ namespace c_sharp_grad_backend.Controllers
             {
                 var values = await connection.QueryFirstAsync<UserProfile>(String.Format("SELECT * FROM TableUserProfiles WHERE Username like '{0}'", donation.Username));
                 cell = values.Cell;
+                
+                firstname = values.Firstname;
             }
+
+             
+            decimal amount = donation.Amount;
+
+            var message = String.Format("Dear {0}, thank you for donating R{1:0.00} to the COVID-19 cause.", firstname , amount);
+
+            var nvc = new List<KeyValuePair<string, string>>();
+            nvc.Add(new KeyValuePair<string, string>("To", cell[0] == '+' ? cell : "+27" + cell.Remove(0,1) ));
+            nvc.Add(new KeyValuePair<string, string>("From", "+17748477045"));
+            nvc.Add(new KeyValuePair<string, string>("Body", message));
+
+            var httpClient = new HttpClient();
+            var encoding = new ASCIIEncoding();
+            var authHeader = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(encoding.GetBytes(string.Format("{0}:{1}", "ACfa70b55ad0f6e2fd05d0d41f5f6c8931", "2e1201887185d89077b3ab4b6659b7cb"))));
+            httpClient.DefaultRequestHeaders.Authorization = authHeader;
+
+            var req = new HttpRequestMessage(HttpMethod.Post, "https://api.twilio.com/2010-04-01/Accounts/ACfa70b55ad0f6e2fd05d0d41f5f6c8931/Messages.json") { Content = new FormUrlEncodedContent(nvc) };
+            var response = await httpClient.SendAsync(req);
+
 
             return StatusCode(201);
         }
